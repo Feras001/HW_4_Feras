3015f10afc603b85f777bce63eff4ac639aaa05c
Feras001 <ferasshaker@mail.ru>
2020-08-02 12:37:36 +0300

    Initial Commit

---
+++ HW_4_Feras/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HW_4_Feras
+//
+//  Created by Feras Shaker on 02.08.2020.
+//  Copyright © 2020 Feras Shaker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
